4c2066ef6110d3840caca420b262a37684a67061
Adrian Pałubicki <67241647+LewnaJa@users.noreply.github.com>
2025-05-16 18:04:22 +0200

    Ferro
    Framework

---
+++ setup.py
@@ -0,0 +1,19 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="ferro-framework",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "flask>=2.0.0",
+        "sqlalchemy>=1.4.0",
+        "flask-cors>=3.0.0",
+        "flask-socketio>=5.0.0",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ferro=packages.cli.ferro:main',
+        ],
+    },
+)
